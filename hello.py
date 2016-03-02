d9db05439a46e9bb26418520f17b55b5e0336207
goalong <goforalong@gmail.com>
2016-03-02 21:36:54 +0800

    test

--- hello.py
+++ hello.py
@@ -1,33 +1,33 @@
 import time
-from flask import Flask,g,request,make_response
+from flask import Flask, request, make_response
 import hashlib
 import xml.etree.ElementTree as ET
+
 app = Flask(__name__)
-app.debug=True
-token='test'
-@app.route('/wechat',methods=['GET','POST'])
+
+@app.route('/wechat', methods=['GET', 'POST'])
 def wechat_auth():
     if request.method == 'GET':
-
-        data = request.args
-        signature = data.get('signature','')
-        timestamp = data.get('timestamp','')
-        nonce = data.get('nonce','')
-        echostr = data.get('echostr','')
-        s = [timestamp,nonce,token]
+        token = 'test'  
+        query = request.args
+        signature = query.get('signature', '')
+        timestamp = query.get('timestamp', '')
+        nonce = query.get('nonce', '')
+        echostr = query.get('echostr', '')
+        s = [timestamp, nonce, token]
         s.sort()
         s = ''.join(s)
         if (hashlib.sha1(s).hexdigest() == signature):
             return make_response(echostr)
     else:
-        rec = request.stream.read()
-        xml_rec = ET.fromstring(rec)
-        tou = xml_rec.find('ToUserName').text
-        fromu = xml_rec.find('FromUserName').text
-        content = xml_rec.find('Content').text
-        xml_rep = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
-        response = make_response(xml_rep % (fromu,tou,str(int(time.time())), content))
-        response.content_type='application/xml'
+        xml_recv = ET.fromstring(request.data)
+        ToUserName = xml_recv.find("ToUserName").text
+        FromUserName = xml_recv.find("FromUserName").text
+        Content = xml_recv.find("Content").text
+        reply = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
+        response = make_response(reply % (FromUserName, ToUserName,
+                                          str(int(time.time())), Content))
+        response.content_type = 'application/xml'
         return response
 
 if __name__ == '__main__':
