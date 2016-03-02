228aad135740e853ecc243e4901a350731cc20b5
goalong <goforalong@gmail.com>
2016-03-03 00:37:02 +0800

    test

--- weixinInterface.py
+++ weixinInterface.py
@@ -16,25 +16,23 @@ class WeixinInterface:
         self.render = web.template.render(self.templates_root)
  
     def GET(self):
-
         data = web.input()
-        if len(data) >= 2:
-            # pdb.set_trace()
-            signature=data.get(signature)
-            timestamp=data.get(timestamp)
-            nonce=data.get(nonce)
-            echostr=data.get(echostr)
+        signature=data.get('signature')
+        timestamp=data.get('timestamp')
+        nonce=data.get('nonce')
+        echostr=data.get('echostr')
 
-            token="test"
+        token="test"
 
+        if len(data) >= 2 and signature and timestamp and nonce:
+            
             l=[token,timestamp,nonce]
             l.sort()
             s = l[0] + l[1] + l[2]
-     
-
             if hashlib.sha1(s).hexdigest() == signature:
-                return 'verify'
-        return 'not verify'
+                return echostr
+        # pdb.set_trace()
+        return data
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
