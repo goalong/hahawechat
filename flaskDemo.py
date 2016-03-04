098c057a0dda847b5bf0f5683b733df03f009a91
goalong <goforalong@gmail.com>
2016-03-04 14:42:38 +0800

    fix

--- flaskDemo.py
+++ flaskDemo.py
@@ -1,8 +1,7 @@
 # encoding: utf-8
 
 import time
-# from flask import Flask, request, make_response
-from bottle import Bottle, request, response
+from flask import Flask, request, make_response
 import hashlib
 import xml.etree.ElementTree as ET
 from settings import APPID, APPSECRET, SCOPE, REDIRECT_URI, URL
@@ -11,15 +10,14 @@ import json
 from webpyDemo import get_userinfo
 import pdb
 
-# app = Flask(__name__)
-app = Bottle()
+app = Flask(__name__)
 app.debug=True
 # 服务器验证
 @app.route('/wechat', methods=['GET', 'POST'])
 def wechat_verify():
     if request.method == 'GET':
         token = 'test'  
-        query = request.query
+        query = request.args
         signature = query.get('signature', '')
         timestamp = query.get('timestamp', '')
         nonce = query.get('nonce', '')
@@ -31,7 +29,7 @@ def wechat_verify():
             return echostr
         return u'验证失败'
     else:
-        xml_recv = ET.fromstring(request.params)    #XML数据存储在request.data中
+        xml_recv = ET.fromstring(request.data)    #XML数据存储在request.data中
         ToUserName = xml_recv.find("ToUserName").text
         FromUserName = xml_recv.find("FromUserName").text
         Content = xml_recv.find("Content").text
@@ -39,21 +37,21 @@ def wechat_verify():
                     <FromUserName><![CDATA[%s]]></FromUserName>
                     <CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType>
                     <Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>'''
-        response = reply % (FromUserName, ToUserName,
-                                          str(int(time.time())), Content)
+        response = make_response(reply % (FromUserName, ToUserName,
+                                          str(int(time.time())), Content))
         response.content_type = 'application/xml'
         return response
 # 网页授权
 @app.route('/auth', methods=['GET', 'POST'])
 def auth():
     if request.method == 'GET':
-        code = request.query.get('code', '')
+        code = request.args.get('code', '')
         # userinfo = get_userinfo(code)
         url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid={APPID}&secret={SECRET}&code={CODE}&grant_type=authorization_code'.format(
                     APPID=APPID, SECRET=APPSECRET, CODE=code)
         content = urllib2.urlopen(url).read()
         content = json.loads(content)
-        return content.keys()
+        return content.keys(), content.values()
         # access_token = content.get('access_token', '')
         # openid = content.get('openid')
         # url2 = 'https://api.weixin.qq.com/sns/userinfo?access_token={ACCESS_TOKEN}&openid={OPENID}&lang=zh_CN'.format(ACCESS_TOKEN=access_token, OPENID=openid)
