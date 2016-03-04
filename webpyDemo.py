6656169ff34695f9dc762a288da0ad95e9e72a6f
goalong <goforalong@gmail.com>
2016-03-04 16:07:57 +0800

    fix

--- webpyDemo.py
+++ webpyDemo.py
@@ -54,19 +54,22 @@ class Auth():
             return render.auth(userinfo['nickname'], userinfo['city'], userinfo['country'], userinfo['province'])
         except Exception as e:
             return e
+def get_dict(url):
+    '''输入url, 返回由json转成的python字典'''
+
+    return json.loads(urllib2.urlopen(url).read())
 
 def get_userinfo(code):
     url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid={APPID}&secret={SECRET}&code={CODE}&grant_type=authorization_code'.format(
                     APPID=APPID, SECRET=APPSECRET, CODE=code)
-    content = urllib2.urlopen(url).read()
-    content = json.loads(content)
+    content = get_dict(url)
     access_token = content.get('access_token', '')
     refresh_token = content.get('refresh_token', '')
     openid = content.get('openid', '')
     refresh_url = 'https://api.weixin.qq.com/sns/oauth2/refresh_token?appid={APPID}&grant_type=refresh_token&refresh_token={REFRESH_TOKEN}'.format(APPID=APPID, REFRESH_TOKEN=refresh_token)
-    refresh_access_token = json.loads(urllib2.urlopen(refresh_url).read()).get('access_token', '')
-    url3 = 'https://api.weixin.qq.com/sns/userinfo?access_token={ACCESS_TOKEN}&openid={OPENID}&lang=zh_CN'.format(ACCESS_TOKEN=refresh_access_token, OPENID=openid)
-    userinfo = json.loads(urllib2.urlopen(url3).read())
+    refresh_access_token = get_dict(refresh_url).get('access_token', '')
+    userinfo_url = 'https://api.weixin.qq.com/sns/userinfo?access_token={ACCESS_TOKEN}&openid={OPENID}&lang=zh_CN'.format(ACCESS_TOKEN=refresh_access_token, OPENID=openid)
+    userinfo = get_dict(userinfo_url)
     return userinfo
 
 
