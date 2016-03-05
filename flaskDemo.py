0d43e8577c0b4ee923b3c992c02653fd90bc556d
goalong <goforalong@gmail.com>
2016-03-05 13:48:56 +0800

    fix

--- flaskDemo.py
+++ flaskDemo.py
@@ -42,7 +42,7 @@ def wechat_verify():
                     <FromUserName><![CDATA[%s]]></FromUserName>
                     <CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType>
                     <Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>'''
-        reply_list = [u'呵呵', u'干嘛', u'去洗澡。。。', u'你说什么，大声点', u'怎么回复呢？？？'］
+        reply_list = [u'呵呵', u'干嘛', u'去洗澡。。。', u'你说什么，大声点']
         response = make_response(reply % (FromUserName, ToUserName,
                                           str(int(time.time())), random.choice(reply_list)))
         response.content_type = 'application/xml'
